26eff117c137549b68988498250bf20a94cee829
bingzz <bingz.real@gmail.com>
2025-05-14 15:07:16 +0800

    Home page in progress + added education, work experience + new icons/images + Changed the browser icon

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,8 +1,182 @@
+import face from '../assets/img/face.jpg';
+import ga from '../assets/img/ga.png';
+import uc from '../assets/img/uc.jpg';
+import tesda from '../assets/img/tesda.jpg';
+import phs from '../assets/img/phs.png';
+
+
 export const personal_info = {
+  name: 'Irving Craig Real',
   email: 'bingz.real@gmail.com',
   location: 'Cebu, Philippines',
   contact: '(+63) 933-055-3684',
-
+  img: face,
+  education: [
+    {
+      name: 'University of Cebu - Banilad',
+      achievement: 'Bachelor of Science in Information Technology',
+      icon_src: uc,
+      length: 'Oct 2017 - Sep 2022'
+    },
+    {
+      name: 'Technical Education and Skills Development Authority (TESDA)',
+      achievement: 'Electrical Installation NC - II',
+      icon_src: tesda,
+      length: 'Mar 2017 - Jun 2017'
+    },
+    {
+      name: 'Pikesville High School',
+      achievement: 'Higher Secondary School',
+      icon_src: phs,
+      length: 'Sep 2010 - May 2014'
+    },
+  ],
+  experience: [
+    {
+      position: 'Web Developer',
+      company: 'GoodApps Inc.',
+      icon_src: ga,
+      length: 'Nov 2022 - Apr 2025',
+    },
+  ],
+  stack: [
+    {
+      name: 'Javascript',
+      icon_src: '',
+    },
+    {
+      name: 'Typescript',
+      icon_src: '',
+    },
+    {
+      name: 'CSS',
+      icon_src: '',
+    },
+    {
+      name: 'HTML',
+      icon_src: '',
+    },
+    {
+      name: 'Golang',
+      icon_src: '',
+    },
+    {
+      name: 'MySQL',
+      icon_src: '',
+    },
+    {
+      name: 'MongoDB',
+      icon_src: '',
+    },
+    {
+      name: 'PostgreSQL',
+      icon_src: '',
+    },
+    {
+      name: 'C#',
+      icon_src: '',
+    },
+    {
+      name: 'Java',
+      icon_src: '',
+    },
+  ],
+  tools: [
+    {
+      name: 'Git',
+      icon_src: '',
+    },
+    {
+      name: 'Github',
+      icon_src: '',
+    },
+    {
+      name: 'Trello',
+      icon_src: '',
+    },
+    {
+      name: 'PHPMyAdmin',
+      icon_src: '',
+    },
+    {
+      name: 'Visual Studio Code',
+      icon_src: '',
+    },
+    {
+      name: 'Postman',
+      icon_src: '',
+    },
+    {
+      name: 'Insomnia',
+      icon_src: '',
+    },
+    {
+      name: 'DBeaver',
+      icon_src: '',
+    },
+    {
+      name: 'Docker',
+      icon_src: '',
+    },
+    {
+      name: 'Adobe Lightroom',
+      icon_src: '',
+    },
+    {
+      name: 'Adobe Premiere',
+      icon_src: '',
+    },
+    {
+      name: 'Adobe Photoshop',
+      icon_src: '',
+    },
+    {
+      name: 'Canva',
+      icon_src: '',
+    },
+  ],
+  frameworks: [
+    {
+      name: 'Angular',
+      icon_src: '',
+    },
+    {
+      name: 'React',
+      icon_src: '',
+    },
+    {
+      name: 'NodeJS',
+      icon_src: '',
+    },
+    {
+      name: 'ExpressJS',
+      icon_src: '',
+    },
+    {
+      name: 'Svelte',
+      icon_src: '',
+    },
+    {
+      name: 'jQuery',
+      icon_src: '',
+    },
+    {
+      name: 'Sass',
+      icon_src: '',
+    },
+    {
+      name: 'TailwindCSS',
+      icon_src: '',
+    },
+    {
+      name: 'Bootstrap',
+      icon_src: '',
+    },
+    {
+      name: 'DaisyUI',
+      icon_src: '',
+    },
+  ]
 };
 
 export const apiKeys = {
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,113 @@
+import { personal_info } from "../data/constants";
+import educ from '../assets/lottie/education.json';
+import work from '../assets/lottie/work_experience.json';
+import LottieTemplate from "../components/Lottie";
+
 export default function Home() {
   return (
-    <section id="home">
-      
-    </section>
-  )
+    <main id="home">
+      <Hero />
+      <AboutMe />
+      <TechStack />
+      <Experience />
+      <Education />
+    </main>
+  );
+}
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="face">
+        <span className="img"></span>
+      </div>
+      <div className="heading">
+        <h1>{personal_info.name}</h1>
+        <div>
+          <h3>Junior Software Engineer</h3>
+          <i>– Progression comes naturally when you're having fun.</i>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function AboutMe() {
+  return (
+    <div className="about">
+      <h3>About Me</h3>
+      <hr />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nemo, eos autem temporibus quibusdam vel laudantium quisquam quae harum sint provident eaque iure magnam explicabo at magni maiores reprehenderit exercitationem natus laborum. Cumque dolores excepturi unde! Nihil quia animi voluptate saepe nisi dolor, possimus aut aspernatur autem! Odio iusto illum, unde eaque mollitia quisquam, odit amet sequi cumque numquam, adipisci et dolores? Pariatur, molestias quis. Praesentium minima ipsam architecto mollitia non. Deserunt odio eos illo voluptatum amet non esse fugiat ex repudiandae. Necessitatibus rerum atque excepturi omnis earum quasi saepe veniam reiciendis eaque, provident, repellendus veritatis deleniti nesciunt incidunt vitae!</p>
+      <br />
+      <p>Lorem ipsum dolor sit amet.</p>
+      <br />
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex neque quisquam eveniet! Commodi deserunt facere consequuntur exercitationem possimus omnis autem facilis! Voluptatem animi nam incidunt neque magnam, et dolore dicta repellendus aliquam nisi maiores adipisci exercitationem architecto quaerat atque illo sint tempore officiis accusamus eius, praesentium sed. Vero, tempore ipsam!</p>
+    </div>
+  );
+}
+
+function TechStack() {
+  return (
+    <div className="stack">
+      <h3>My Technology Stack</h3>
+      <hr />
+    </div>
+  );
+}
+
+function Experience() {
+  return (
+    <div className="experience">
+      <h3>Work Experience</h3>
+      <hr />
+      <div className="container">
+        <ul className="blocks">
+          {personal_info.experience.map((item, key) => (
+            <li key={key}>
+              <div className="logo">
+                <img src={item.icon_src} alt={`${item.company}_${key}`} />
+              </div>
+              <div className="details">
+                <p className="position">{item.position}</p>
+                <p className="company"><strong>{item.company}</strong></p>
+                <p className="length">{item.length}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="lottie">
+          <LottieTemplate src={work} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Education() {
+  return (
+    <div className="education">
+      <h3>Education</h3>
+      <hr />
+      <div className="container">
+        <div className="lottie">
+          <LottieTemplate src={educ} />
+        </div>
+        <ul className="blocks">
+          {personal_info.education.map((item, key) => (
+            <li key={key}>
+              <div className="logo">
+                <img src={item.icon_src} alt={`${item.name}_${key}`} />
+              </div>
+              <div className="details">
+                <p className="achievement"><strong>{item.achievement}</strong></p>
+                <p className="name">{item.name}</p>
+                <p className="length">{item.length}</p>
+              </div>
+              {/* {key !== personal_info.education.length - 1 ? <span>vertical</span> : null} */}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
