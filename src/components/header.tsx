7a110265847fc841e16207e8a76e7af60551c4bb
bingzz <bingz.real@gmail.com>
2025-05-16 12:24:20 +0800

    Responsive header + Toggle header menu for small devices

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import { MouseEventHandler, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const nav = [
@@ -16,22 +17,68 @@ const nav = [
 ];
 
 export default function Header() {
+  // Toggle menu
+  const [toggle, setToggled] = useState(false);
+
+  const toggleMenu = (e: React.MouseEvent<HTMLButtonElement | HTMLDivElement | HTMLLIElement>) => !!toggle ? setToggled(false) : setToggled(true);
+
   return (
     <header>
       <nav>
-        <h3>Welcome Visitor!</h3>
-        <ul>
+        <h3>Welcome, Stranger</h3>
+        <div className="topbar">
+          <ul className="navigation">
+            {
+              nav.map(item => (
+                <li key={item.path}>
+                  <NavLink to={item.path} className={({ isActive }) => isActive ? 'active' : ''}>
+                    {item.page}
+                  </NavLink>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="sidebar">
+          <button onClick={toggleMenu}><span className="hamburger"></span></button>
+          <ul className={`navigation ${toggle ? 'toggle' : ''}`}>
+            {
+              nav.map(item => (
+                <li key={item.path} onClick={toggleMenu}>
+                  <NavLink to={item.path} className={({ isActive }) => isActive ? 'active' : ''}>
+                    {item.page}
+                  </NavLink>
+                </li>
+              ))
+            }
+          </ul>
+          <div className={`sidebar-overlay ${toggle ? 'toggle' : ''}`} onClick={toggleMenu}></div>
+        </div>
+      </nav>
+      {/* <nav className="topbar">
+        <ul className="navigation">
           {
             nav.map(item => (
               <li key={item.path}>
-                <NavLink to={item.path} className={({isActive}) => isActive ? 'active' : ''}>
-                {item.page}
+                <NavLink to={item.path} className={({ isActive }) => isActive ? 'active' : ''}>
+                  {item.page}
                 </NavLink>
               </li>
             ))
           }
         </ul>
       </nav>
+      <nav className="sidebar">
+        <ul className="sidebar-nav">
+          {nav.map(item => (
+            <li key={item.path}>
+              <NavLink to={item.path} className={({ isActive }) => isActive ? 'active' : ''}>
+                {item.page}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav> */}
     </header>
   );
 }
